0c90361bce10645d53523a19ed10a0630c6e535b
Oscare101 <kirillfedortsev2000@gmail.com>
2024-06-21 11:36:20 +0300

    colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,18 @@
+export default {
+  dark: {
+    bg: '#000000',
+    card: '#1F1F20',
+    main: '#D4DBDF',
+    comment: '#606465',
+    error: '#FF4D4F',
+    accent: '#66BBFF',
+  },
+  light: {
+    bg: '#F2F4F5',
+    card: '#FFFFFF',
+    main: '#374951',
+    comment: '#B6BBBC',
+    error: '#940707',
+    accent: '#2997FF',
+  },
+};
